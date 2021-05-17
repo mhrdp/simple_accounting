6c3aecd8e5bd0e21e08d7667a580861d72742c6f
MhrDP <cumigorengbakar@yahoo.com>
2021-05-17 10:12:07 +0700

    add signals

--- user/signals.py
+++ user/signals.py
@@ -1,15 +1,14 @@
-#from django.db.models.signals import post_save
-#from django.contrib.auth import get_user_model
-#from django.contrib.auth.models import User
-#from django.dispatch import receiver
-#from .models import UserPreferences
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from .models import UserPreferences
 
-#user_model = get_user_model()
-#@receiver(post_save, sender=User)
-#def initialize_setting(sender, instance, created, **kwargs):
-#    if created:
-#        UserPreferences.objects.create(user=instance)
+user_model = get_user_model()
+@receiver(post_save, sender=user_model)
+def initialize_setting(sender, instance, created, **kwargs):
+    if created:
+        UserPreferences.objects.create(username=instance)
 
-#@receiver(post_save, sender=User)
-#def save_default_setting(sender, instance, **kwargs):
-#    instance.userpreferences.save()
+@receiver(post_save, sender=user_model)
+def save_default_setting(sender, instance, **kwargs):
+    instance.userpreferences.save()
